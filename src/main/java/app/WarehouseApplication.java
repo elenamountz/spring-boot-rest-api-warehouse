0a88300f6ca37da3b6a5699a630a7ffabf1c78e3
Elena Mountzia <emountzia@ots.gr>
2020-09-24 19:14:43 +0300

    Initial commit

---
+++ src/main/java/app/WarehouseApplication.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseApplication.class, args);
+	}
+
+}
