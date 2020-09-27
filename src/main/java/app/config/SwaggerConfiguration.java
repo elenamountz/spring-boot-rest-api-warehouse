c698ec8b97940e386462c1a9cbfef076679659a7
Elena Mountzia <el.mountzia@gmail.com>
2020-09-28 00:16:11 +0300

    Setup swagger configuration

---
+++ src/main/java/app/config/SwaggerConfiguration.java
@@ -0,0 +1,22 @@
+package app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("app"))
+                .build();
+    }
+
+}
