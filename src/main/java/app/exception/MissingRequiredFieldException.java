cf83dc983583472b2d09fe235e1931d1af4fae79
Elena Mountzia <el.mountzia@gmail.com>
2020-09-29 16:52:44 +0300

    Add custom exception

---
+++ src/main/java/app/exception/MissingRequiredFieldException.java
@@ -0,0 +1,11 @@
+package app.exception;
+
+import app.common.exception.BaseRuntimeException;
+import org.springframework.http.HttpStatus;
+
+public class MissingRequiredFieldException extends BaseRuntimeException {
+
+    public MissingRequiredFieldException(String message) {
+        super("Field '" + message + "' is required", HttpStatus.NOT_ACCEPTABLE);
+    }
+}
