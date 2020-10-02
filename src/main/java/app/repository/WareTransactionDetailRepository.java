8dac5ac8a7ddec3f6ae59accd63058b89b327a91
Elena Mountzia <el.mountzia@gmail.com>
2020-10-02 23:38:54 +0300

    Add query for total existing quantity

--- src/main/java/app/repository/WareTransactionDetailRepository.java
+++ src/main/java/app/repository/WareTransactionDetailRepository.java
@@ -2,10 +2,19 @@ package app.repository;
 
 import app.model.WareTransactionDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface WareTransactionDetailRepository extends JpaRepository<WareTransactionDetail, Long> {
 
     List<WareTransactionDetail> findByWareTransactionId(Long wareTransactionId);
+
+    @Query("SELECT SUM(detail.quantity) " +
+            "FROM WareTransactionDetail detail " +
+            "WHERE detail.product.id=:productId " +
+            "AND detail.shelf.id=:shelfId")
+    Optional<Integer> findTotalQuantityByProductAndShelf(@Param("productId") Long productId, @Param("shelfId") Long shelfId);
 }
