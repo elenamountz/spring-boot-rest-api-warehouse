0c41cb7e44d9ac0f86f807e6474ad20f4a1e685c
Elena Mountzia <el.mountzia@gmail.com>
2020-10-24 23:30:38 +0300

    Remove query

--- src/main/java/app/repository/WareTransactionDetailRepository.java
+++ src/main/java/app/repository/WareTransactionDetailRepository.java
@@ -2,12 +2,10 @@ package app.repository;
 
 import app.enums.WareTransactionType;
 import app.model.WareTransactionDetail;
-import app.projection.StockProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,22 +23,5 @@ public interface WareTransactionDetailRepository extends JpaRepository<WareTrans
             @Param("productId") Long productId,
             @Param("shelfId") Long shelfId,
             @Param("wareTransactionType") WareTransactionType wareTransactionType);
-
-
-    @Query("SELECT NEW app.projection.StockProjection(" +
-            "wh.description, " +
-            "shelf.code, " +
-            "product.code, " +
-            "SUM(CASE WHEN tx.wareTransactionType='IMPORT' " +
-            "THEN detail.quantity " +
-            "ELSE -detail.quantity END)) " +
-            "FROM WareTransactionDetail detail " +
-            "INNER JOIN WareTransaction tx ON detail.wareTransaction.id=tx.id " +
-            "INNER JOIN Product product ON detail.product.id=product.id " +
-            "INNER JOIN Shelf shelf ON detail.shelf.id=shelf.id " +
-            "INNER JOIN Warehouse wh ON shelf.warehouse.id=wh.id " +
-            "WHERE product.code=:productCode AND tx.transactionDate<=:date " +
-            "GROUP BY wh.description, shelf.code ")
-    List<StockProjection> findStockByProductAndDate(@Param("productCode") String productCode, @Param("date") LocalDateTime date);
 
 }
